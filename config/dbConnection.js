d770ac2fdb9080cf327747011182d8c7804b3de5
Mintah <yawmintah1@gmail.com>
2024-07-09 01:31:19 +0000

    made changes to db uri

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -6,9 +6,10 @@ const {
 POSTGRES_HOST,
 POSTGRES_DB,
 POSTGRES_USER,
-POSTGRES_PASSWORD
+POSTGRES_PASSWORD,
+POSTGRES_URI
 } = process.env
 
-module.exports.sequelize = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}`)
+module.exports.sequelize = new Sequelize(POSTGRES_URI);
 
 
